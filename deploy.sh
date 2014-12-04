dc090079607f8f467ae9faa077840ca1d830c5d6
Kamran Ayub <kamran.ayub@gmail.com>
2014-12-03 19:59:38 -0600

    Add travis deployment script

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+CURRENT_COMMIT=`git rev-parse HEAD`
+
+npm install typescript@1.3.0 -g
+
+echo "Cloning gh-pages branch..."
+
+git clone -b gh-pages "https://${GH_TOKEN}@${GH_REF}" out > /dev/null 2>&1 || exit 1
+
+rm -r out/*
+
+echo "Compiling TSC"
+
+tsc --sourcemap "./Src/game.ts" --out "./Src/game.js"
+
+echo "Copying built files"
+
+cp Src/ out/ -r
+cd out
+rm Src.csproj
+
+echo "Committing and pushing to GH"
+
+git config user.name "Travis-CI"
+git config user.email "travis@excaliburjs.com"
+git add -A
+git commit --allow-empty -m "Deploying game for $CURRENT_COMMIT" || exit 1
+git push origin gh-pages > /dev/null 2>&1 || exit 1
+
+echo "Pushed deployment successfully"
+exit 0
