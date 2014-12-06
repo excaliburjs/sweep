87d22447fb06e5ce4518e4a398957c65b4deb744
Travis-CI <travis@excaliburjs.com>
2014-12-06 19:00:25 +0000

    Deploying game for 3ef174c80f9e6e7b47e47b7e6a55d600715e7742

--- game/util.ts
+++ game/util.ts
@@ -1,11 +1,18 @@
 ﻿class Util {
    
-   public static darken(color: ex.Color, value: number) {
-      var r = Math.floor(color.r - (color.r * value));
-      var g = Math.floor(color.g - (color.g * value));
-      var b = Math.floor(color.b - (color.b * value));
+   public static darken(color: ex.Color, amount: number) {
+      var r = Math.floor(color.r - (color.r * amount));
+      var g = Math.floor(color.g - (color.g * amount));
+      var b = Math.floor(color.b - (color.b * amount));
 
       return new ex.Color(r, g, b, color.a);
    }
 
+   public static lighten(color: ex.Color, amount: number) {
+      var r = Math.min(255, Math.floor(color.r + (255 * amount)));
+      var g = Math.min(255, Math.floor(color.g + (255 * amount)));
+      var b = Math.min(255, Math.floor(color.b + (255 * amount)));
+
+      return new ex.Color(r, g, b, color.a);
+   }
 } 
