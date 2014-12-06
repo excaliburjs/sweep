45ce290b583c634a533d1ac4beb5a903dc530432
Travis-CI <travis@excaliburjs.com>
2014-12-06 18:06:04 +0000

    Deploying game for e9d4378b9c483abe03c279531e85f7526f31f06a

--- game/Config.ts
+++ game/Config.ts
@@ -1,10 +1,12 @@
 ﻿class Config {
 
-   static PieceWidth: number = 20;
-   static PieceHeight: number = 20;
-   static CellWidth: number = 30;
-    static CellHeight: number = 30;
-    static GridCellsHigh: number = 15;
-    static GridCellsWide: number = 10;
+   static gameWidth: number = 1440;
+   static gameHeight: number = 900;
+   static PieceWidth: number = 40;
+   static PieceHeight: number = 40;
+   static CellWidth: number = 50;
+   static CellHeight: number = 50;
+   static GridCellsHigh: number = 15;
+   static GridCellsWide: number = 10;
 
 }
