14573aad8905e6b0c57be12842c787f73ee7bc3b
eonarheim <erik.onarheim@gmail.com>
2014-12-13 00:09:05 -0600

    Fix memory and tile breaking bugs

--- Src/game/Piece.ts
+++ Src/game/Piece.ts
@@ -8,6 +8,12 @@
 var PieceTypes = [PieceType.Circle, PieceType.Square, PieceType.Triangle, PieceType.Star];
 var PieceTypeToColor = [Palette.PieceColor1, Palette.PieceColor2, Palette.PieceColor3, Palette.PieceColor4];
 var PieceTypeToTexture = [Resources.TextureTile1, Resources.TextureTile2, Resources.TextureTile3, Resources.TextureTile4];
+var PieceTypeToSprites = [
+   [new ex.Sprite(PieceTypeToTexture[0], 0, 0, Config.PieceWidth, Config.PieceHeight), new ex.Sprite(PieceTypeToTexture[0], Config.PieceWidth, 0, Config.PieceWidth, Config.PieceHeight), new ex.Sprite(PieceTypeToTexture[0], Config.PieceWidth * 2, 0, Config.PieceWidth, Config.PieceHeight)],
+   [new ex.Sprite(PieceTypeToTexture[1], 0, 0, Config.PieceWidth, Config.PieceHeight), new ex.Sprite(PieceTypeToTexture[1], Config.PieceWidth, 0, Config.PieceWidth, Config.PieceHeight), new ex.Sprite(PieceTypeToTexture[1], Config.PieceWidth * 2, 0, Config.PieceWidth, Config.PieceHeight)],
+   [new ex.Sprite(PieceTypeToTexture[2], 0, 0, Config.PieceWidth, Config.PieceHeight), new ex.Sprite(PieceTypeToTexture[2], Config.PieceWidth, 0, Config.PieceWidth, Config.PieceHeight), new ex.Sprite(PieceTypeToTexture[2], Config.PieceWidth * 2, 0, Config.PieceWidth, Config.PieceHeight)],
+   [new ex.Sprite(PieceTypeToTexture[3], 0, 0, Config.PieceWidth, Config.PieceHeight), new ex.Sprite(PieceTypeToTexture[3], Config.PieceWidth, 0, Config.PieceWidth, Config.PieceHeight), new ex.Sprite(PieceTypeToTexture[3], Config.PieceWidth * 2, 0, Config.PieceWidth, Config.PieceHeight)]
+];
 
 class PieceEvent extends ex.GameEvent {
    constructor(public cell: Cell) {
@@ -51,14 +57,18 @@ class Piece extends ex.Actor {
    }
 
    private _updateDrawings() {
-      var tileSprite = new ex.Sprite(PieceTypeToTexture[this._type], 0, 0, Config.PieceWidth, Config.PieceHeight);
-      this.addDrawing("default", tileSprite);
+      //var tileSprite = new ex.Sprite(PieceTypeToTexture[this._type], 0, 0, Config.PieceWidth, Config.PieceHeight);
+      //this.addDrawing("default", tileSprite);
+      this.addDrawing("default", PieceTypeToSprites[this._type][0]);
 
-      var highlightSprite = new ex.Sprite(PieceTypeToTexture[this._type], Config.PieceWidth, 0, Config.PieceWidth, Config.PieceHeight);
-      this.addDrawing("highlight", highlightSprite);
+      //var highlightSprite = new ex.Sprite(PieceTypeToTexture[this._type], Config.PieceWidth, 0, Config.PieceWidth, Config.PieceHeight);
+      //this.addDrawing("highlight", highlightSprite);
+      this.addDrawing("highlight", PieceTypeToSprites[this._type][1]);
 
-      var fadedSprite = new ex.Sprite(PieceTypeToTexture[this._type], Config.PieceWidth * 2, 0, Config.PieceWidth, Config.PieceHeight);
-      this.addDrawing("faded", fadedSprite);
+      //var fadedSprite = new ex.Sprite(PieceTypeToTexture[this._type], Config.PieceWidth * 2, 0, Config.PieceWidth, Config.PieceHeight);
+      //this.addDrawing("faded", fadedSprite);
+      this.addDrawing("faded", PieceTypeToSprites[this._type][2]);
+      this.setDrawing("default");
    }
 
    public onInitialize(engine: ex.Engine) {
@@ -77,13 +87,17 @@ class Piece extends ex.Actor {
          this.setDrawing("default");
       }
    }
+
+   public dispose() {
+
+   }
 }
 
 class PieceFactory {
    private static _maxId: number = 0;
    public static getRandomPiece(x: number = 0, y: number = 0): Piece {
       var index = Math.floor(Math.random() * PieceTypes.length);
-      var piece = new Piece(PieceFactory._maxId++, x, y, PieceTypeToColor[index].clone(), index);
+      var piece = new Piece(PieceFactory._maxId++, x, y, PieceTypeToColor[index], index);
 
       
       game.add(piece);
@@ -93,7 +107,7 @@ class PieceFactory {
 
    public static getPiece(type: PieceType) {
       
-      var piece = new Piece(PieceFactory._maxId++, 0, 0, PieceTypeToColor[type].clone(), type);
+      var piece = new Piece(PieceFactory._maxId++, 0, 0, PieceTypeToColor[type], type);
 
       game.add(piece);
 
