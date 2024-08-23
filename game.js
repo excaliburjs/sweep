82fe19095ff3ba98c168853f1c7d349f708d1418
Erik Onarheim <erik.onarheim@gmail.com>
2024-08-22 23:14:30 -0500

    update for cb friendliness

--- game.js
+++ game.js
@@ -212,10 +212,10 @@ var Palette = {
     GameBackgroundColor: ex.Color.fromHex("#efefef"),
     GridBackgroundColor: new ex.Color(0, 20, 25, 0.9),
     // Beach
-    PieceColor1: ex.Color.fromHex("#00718D"),
-    PieceColor2: ex.Color.fromHex("#7A5CA7"),
-    PieceColor3: ex.Color.fromHex("#4c603a"),
-    PieceColor4: ex.Color.fromHex("#c17b55"),
+    PieceColor1: ex.Color.fromHex("#006FAA"),
+    PieceColor2: ex.Color.fromHex("#CC79A7"),
+    PieceColor3: ex.Color.fromHex("#009E73"),
+    PieceColor4: ex.Color.fromHex("#F0E442"),
     MegaSweepColor: ex.Color.fromHex("#55c192"),
     PolylineColor: ex.Color.fromHex("#F48347"),
     PolylineBorderColor: new ex.Color(255, 255, 255, 0.7)
